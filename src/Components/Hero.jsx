55f2f29c6893c3745b41927691b8a3c0a07bc362
Muhammad Diyan Irfan <diyanirfan56@gmail.com>
2026-05-01 21:52:10 +0500

    j

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
           mx-auto 
           bg-cover 
           bg-center md:bg-center
-          bg-[position:75%_center] md:bg-center
+          bg-[position:90%_center] md:bg-center
         "
         style={{ backgroundImage: `url(${doctorImage})` }}
       >
